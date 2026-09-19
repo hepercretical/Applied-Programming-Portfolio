71d9d17f0fb8c6fcc2e14ace6ab9946711c2fa24
Talmage Trebilcock <99899171+hepercretical@users.noreply.github.com>
2026-09-18 20:37:46 -0600

    A very important Hello World Comment

--- Hello World Test Module/HelloWorld.py
+++ Hello World Test Module/HelloWorld.py
@@ -1,1 +1,4 @@
+# This is a simple program that prints "Hello, World!" in the console when a user runs the program.
+# The main purpose of this program in this repo is the ensure that Git and GitHub are working properly
+# and that assignments can be submitted via GitHub in the future.
 print("Hello, World!")
